64b128974d74c3b5bb3c5118500570a24c498c08
blackieghoster <blackieghoster@gmail.com>
2024-03-10 23:23:49 +0100

    Created first model of a product and configured admin.py for it. + Minor settings.py adjustments.

--- szkolny_market_app/models.py
+++ szkolny_market_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+CATEGORY_CHOICES = (
+    [0, "Jedzenie"],
+    [1, "Napoje"],
+)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=32, verbose_name="Nazwa produktu")
+    price = models.DecimalField(max_digits=3, decimal_places=2, verbose_name="Cena produktu")
+    quantity = models.IntegerField(default=0, verbose_name="Ilosć")
+    category = models.IntegerField(choices=CATEGORY_CHOICES, verbose_name="Kategoria")
+    description = models.TextField(verbose_name="Opis produktu", null=True, blank=True)
+
+    def __str__(self):
+        return self.name
--- wsb_team_project/settings.py
+++ wsb_team_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'szkolny_market_app',
 ]
 
 MIDDLEWARE = [
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pl-pl'
 
 TIME_ZONE = 'UTC'
 
