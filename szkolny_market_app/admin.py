118ea14990eddab93ea50b8cfb93e1bc01d45ab3
talos-sq <49106134+talos-sq@users.noreply.github.com>
2024-03-11 20:28:49 +0100

    Created first model of a product and configured admin.py for it. + Minor settings.py adjustments. (#3)

--- szkolny_market_app/admin.py
+++ szkolny_market_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.site_header = "Sklepikowy panel administracyjny"
+admin.site.site_title = "Sklepikowy panel administracyjny"
+
+
+@admin.register(Product)
+class AbilityAdmin(admin.ModelAdmin):
+    model = Product
+
+    list_display = []
