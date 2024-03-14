c413418c6c46740046608e769440f403254cb2eb
blackieghoster <blackieghoster@gmail.com>
2024-03-14 19:07:09 +0100

    Created urls and views.

--- szkolny_market_app/views.py
+++ szkolny_market_app/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class IndexView(View):
+    def get(self, request):
+        return render(request, '')
+
+
+class StudentLoginView(View):
+    def get(self, request):
+        return render(request, '')
+
+
+class ParentLoginView(View):
+    def get(self, request):
+        return render(request, '')
+
+
+class WorkerLoginView(View):
+    def get(self, request):
+        return render(request, '')
+
+
+class StudentMenuView(View):
+    def get(self, request):
+        return render(request, '')
+
+
+class ParentMenuView(View):
+    def get(self, request):
+        return render(request, '')
+
+
+class WorkerMenuView(View):
+    def get(self, request):
+        return render(request, '')
+
+
+class ShopProductsView(View):
+    def get(self, request):
+        return render(request, '')
