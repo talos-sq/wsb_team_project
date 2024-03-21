f80f41cde12ad57e68f6bd163ef7c38719ba4e2a
talos-sq <49106134+talos-sq@users.noreply.github.com>
2024-03-21 18:37:37 +0000

    Login system (#12)

--- szkolny_market_app/views.py
+++ szkolny_market_app/views.py
@@ -1,40 +1,53 @@
+from django.contrib.auth import authenticate, login, get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 
 
 class IndexView(View):
     def get(self, request):
-        return render(request, '')
+        return render(request, 'index.html')
+
+    def post(self, request):
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        if username is not None and password is not None:
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+
+            user = get_user_model().objects.get(username=username)
+            if user.has_perm("szkolny_market_app.student"):
+                return HttpResponseRedirect('student_menu/')
+            elif user.has_perm("szkolny_market_app.parent"):
+                return HttpResponseRedirect('parent_menu/')
+            elif user.has_perm("szkolny_market_app.worker"):
+                return HttpResponseRedirect('worker_menu/')
+            else:
+                return render(request, "index.html")
+        return render(request, "index.html")
 
 
-class StudentLoginView(View):
+class StudentMenuView(LoginRequiredMixin, PermissionRequiredMixin, View):
+    permission_required = "szkolny_market_app.student"
+
     def get(self, request):
-        return render(request, '')
+        return render(request, 'student.html')
 
 
-class ParentLoginView(View):
+class ParentMenuView(LoginRequiredMixin, PermissionRequiredMixin, View):
+    permission_required = "szkolny_market_app.parent"
+
     def get(self, request):
-        return render(request, '')
+        return render(request, 'parent.html')
 
 
-class WorkerLoginView(View):
+class WorkerMenuView(LoginRequiredMixin, PermissionRequiredMixin, View):
+    permission_required = "szkolny_market_app.worker"
+
     def get(self, request):
-        return render(request, '')
-
-
-class StudentMenuView(View):
-    def get(self, request):
-        return render(request, '')
-
-
-class ParentMenuView(View):
-    def get(self, request):
-        return render(request, '')
-
-
-class WorkerMenuView(View):
-    def get(self, request):
-        return render(request, '')
+        return render(request, 'worker.html')
 
 
 class ShopProductsView(View):
